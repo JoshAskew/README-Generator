03f39ea695a1f06e21d79b9633b80fb07a87e407
JoshAskew <asksprit3@gmail.com>
2024-10-08 16:53:04 -0500

    almost completed index.js, still have some bugs to work out.

--- index.js
+++ index.js
@@ -1,12 +1,100 @@
 // TODO: Include packages needed for this application
+import fs from 'fs';
+import inquirer from 'inquirer';
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type: 'input',
+        name: 'title',
+        message: 'What is the title of your project?'
+    },
+    {
+        type: 'input',
+        name: 'description',
+        message: 'Please provide a description of your project:'
+    },
+    {
+        type: 'input',
+        name: 'installation',
+        message: 'What are the installation instructions?'
+    },
+    {
+        type: 'input',
+        name: 'usage',
+        message: 'How is the project used?'
+    },
+    {
+        type: 'input',
+        name: 'contribution',
+        message: 'What are the contribution guidelines?'
+    },
+    {
+        type: 'list',
+        name: 'license',
+        message: 'What is the license type?',
+        choices: [
+            'MIT',
+            'Apache 2.0',
+            'GPL 3.0',
+            'BSD 3-Clause'
+        ]
+    },
+    {
+        type: 'input',
+        name: 'github',
+        message: 'What is your Github username?'
+    },
+    {
+        type: 'input',
+        name: 'email',
+        message: 'What is your E-mail address?'
+    },
+];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+    fs.writeFile(fileName, data, (err) => {
+        if (err) {
+            console.error('Error writing to file:', err);
+        } else {
+            console.log('README file has been generated successfully!');
+        }
+    });
+}
 
 // TODO: Create a function to initialize app
-function init() {}
+async function init() {
+    try {
+        const answers = await inquirer.prompt(questions);
+        const content = `
+# ${answers.title}
+
+## Description
+${answers.description}
+
+## Installation
+${answers.installation}
+
+## Usage
+${answers.usage}
+
+## Contribution
+${answers.contribution}
+
+## License
+${answers.license}
+
+## GitHub
+${answers.github}
+
+## Questions
+${answers.email}
+        `;
+        writeToFile('README.md', content);
+    } catch (error) {
+        console.error('Error initializing app:', error);
+    }
+}
 
 // Function call to initialize app
 init();
